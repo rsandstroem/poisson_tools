b2037869e63334575a6e924ec0dacb331215eabe
rsandstroem <rsandstroem@kpmg.com>
2019-05-10 17:44:30 +0200

    Unit tests for cost function

--- poisson_tools/common.py
+++ poisson_tools/common.py
@@ -3,4 +3,20 @@ from scipy.optimize import minimize
 from numpy import square
 
 def cost_function(mu, n, target):
+    '''
+    Calculates the squared distance between the
+    survival function (1-cdf) of a poisson function 
+    with rate mu and n observations and a 
+    the target value.
+    
+    Arguments:
+        mu {float} -- Event rate, a.k.a. rate parameter
+        n {int} -- Number of observed events
+        target {float} -- Estimated/desired survival rate 
+    
+    Returns:
+        float -- Squared distance between the survival rate 
+        and the target
+    '''
+
     return square(poisson.sf(n, mu)-target)
--- tests/unit/test_common.py
+++ tests/unit/test_common.py
@@ -1,4 +1,15 @@
 from poisson_tools import common
 
 def test_cost_function():
-    assert common.cost_function(1,1,1) == 0
+
+    # 1 sigma low limit
+    assert common.cost_function(2.84,4,0.16) < 1e-4
+
+    # 2 sigma low limit
+    assert common.cost_function(1.62,4,0.025) < 1e-4
+
+    # 1 sigma upper limit
+    assert common.cost_function(7.14,4,0.84) < 1e-4
+
+    # Outside limits
+    assert common.cost_function(20,4,0.84) > 1e-2
